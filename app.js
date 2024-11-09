6b2684108baac6d54ecefc7fefe60d926d8b8503
saugatadk <adhikarisaugat55@gmail.com>
2024-11-09 14:18:02 +0545

    Used bcrypt Hashing

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -37,35 +37,39 @@ app.get("/logout", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
 
-    async function saveUser(){
-        try{
-            await newUser.save();
-            res.render("secrets");
+        async function saveUser(){
+            try{
+                await newUser.save();
+                res.render("secrets");
+            }
+            catch{
+                console.log(err);
+            }
         }
-        catch{
-            console.log(err);
-        }
-    }
-    saveUser();
+        saveUser();
+    });
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
     .then((foundUser)=>{
         if(foundUser){
-           if(foundUser.password === password){
-                res.render("secrets");
-            }else{
-                res.render("login");
-            } 
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets");
+                }else{
+                    res.render("login");
+                }
+            });
         }else{
             res.render("User NOT FOUND!");
         }
