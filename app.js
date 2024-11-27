39705377808024f02a565272a378c52f06271357
saugatadk <adhikarisaugat55@gmail.com>
2024-11-27 15:40:16 +0545

    minor changes

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://secrets-03xx.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
 
